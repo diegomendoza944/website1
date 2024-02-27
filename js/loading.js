4a6ecaaec0654047a0997dc6356de5ecaa156d57
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-02-27 17:57:29 +0000

    Tue, Feb 27, 2024, 9:57 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,12 @@
 loadText = document.querySelector(',loading.text')
-
+bg = document.querySelector('.bg')
 load = 0
 
-loadText.innerText = '${load}%'
-loadText.style.opacity =
+
+
+fuction blurring (){
+    load = load + 1
+    loadText.innerText = '${load}%'
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
