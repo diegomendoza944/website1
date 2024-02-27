eef9687aa04865d2cd7daaa8387f84a0be1b2c18
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-02-27 18:24:28 +0000

    Tue, Feb 27, 2024, 10:24 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,11 @@
-loadText = document.querySelector(',loading.text')
+loadText = document.querySelector(',loading-text')
 bg = document.querySelector('.bg')
 load = 0
 
 int = setInterval(blurring, 50)
 
 fuction blurring (){
-    load = load + 1
+    load++
 
     if (load > 99){
         clearInterval(int)
