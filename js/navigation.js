6e5fcc11e6c8a4596d22fd22eaf1167a8fa3620f
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-03-06 17:05:56 +0000

    Wed, Mar 6, 2024, 9:05 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -2,6 +2,7 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav(){
+    console.log(window.scrollY)
     if (window.scrollY > 223) {
         nav.classList.add('active')
     }
