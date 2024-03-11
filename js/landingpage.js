e2596b9515987485ec606ccca94c14af187f2aa4
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-03-11 17:02:28 +0000

    Mon, Mar 11, 2024, 10:02 AM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,11 +1,16 @@
 toggle = document.getElementById('toggle')
 open = document.getElementById('open')
 modal = document.getElementById('model')
+close = document.getElementById('close')
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
 open.addEventListener('click', () => {
-    modal.classList.add('show-modal)
+    modal.classList.add('show-modal')
+})
+
+close.addEventListener('click'. () => {
+    modal.classList.remove('show-modal')
 })
