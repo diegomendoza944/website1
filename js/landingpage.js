9fb5af48abb9b8a47b1e6796f1bf79dbbd939d6b
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-03-11 16:57:06 +0000

    Mon, Mar 11, 2024, 9:57 AM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,6 +1,11 @@
 toggle = document.getElementById('toggle')
-
+open = document.getElementById('open')
+modal = document.getElementById('model')
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal)
 })
